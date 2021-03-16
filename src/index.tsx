881ed3d4c55baea1f8d00f7a02c34a67d7727521
Kameron <kamahler@gmail.com>
2021-03-16 15:13:14 -0600

    type a useState hook

--- src/index.tsx
+++ src/index.tsx
@@ -1,31 +1,45 @@
 import * as React from 'react'
+import { useState } from 'react'
 import * as ReactDOM from 'react-dom'
-// import App from './components/App/App'
 
+/* simple typing with option */
 // type Test = 'yes' | 'no' | null
 // const tryTest: Test = 'yes'
 
-interface objTest {
-  description: string | null
-  id: number
-  quantity: number
-  rating: number | null
-  title: string
-  [key: string]: any
-}
+/* interface */
+// interface objTest {
+//   description: string | null
+//   id: number
+//   quantity: number
+//   rating: number | null
+//   title: string
+//   [key: string]: any
+// }
 
-const tryObjTest: objTest = {
-  description: '',
-  id: 1234,
-  quantity: 0,
-  rating: 4.2,
-  title: 'Testing Item',
-  outOfStockMessage: `We'll update this soon`,
+// const tryObjTest: objTest = {
+//   description: '',
+//   id: 1234,
+//   quantity: 0,
+//   rating: 4.2,
+//   title: 'Testing Item',
+//   outOfStockMessage: `We'll update this soon`,
+// }
+
+/* function */
+function Test() {
+  const [test, setTest] = useState<null | number>(null)
+
+  return (
+    <>
+      <button onClick={() => setTest(test + 1)}>+</button>
+      <p>{test}</p>
+    </>
+  )
 }
 
 ReactDOM.render(
   <div>
-    <p>{JSON.stringify(tryObjTest)}</p>
+    <Test />
   </div>,
   document.getElementById('root')
 )
