2aad58d713a5187e3f5eca7ee64e9624fd746c91
Kameron <kamahler@gmail.com>
2021-03-16 14:12:53 -0600

    try an interface def

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,30 @@ import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 // import App from './components/App/App'
 
-type Test = 'yes' | 'no' | null
-const tryTest: Test = 'yes'
+// type Test = 'yes' | 'no' | null
+// const tryTest: Test = 'yes'
+
+interface objTest {
+  description: string | null
+  id: number
+  quantity: number
+  rating: number | null
+  title: string
+  [key: string]: any
+}
+
+const tryObjTest: objTest = {
+  description: '',
+  id: 1234,
+  quantity: 0,
+  rating: 4.2,
+  title: 'Testing Item',
+  outOfStockMessage: `We'll update this soon`,
+}
 
 ReactDOM.render(
   <div>
-    <p>{tryTest}</p>
+    <p>{JSON.stringify(tryObjTest)}</p>
   </div>,
   document.getElementById('root')
 )
